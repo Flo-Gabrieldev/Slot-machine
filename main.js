63a266c8cd051caba6c2e9aeba9b5264527dca2d
Gabriel Flores <cybergabriel36@gmail.com>
2024-10-13 13:06:03 -0700

    added a fruitValues function that outputs three random fruits, while altering numToFruit and randomNum function to work within run function

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 // if not in fruits, is a dud!
-fruits = ["lemon", "apple", "pineapple", "watermelon", "pear", "mango", "dragonfruit","golden apple"];
-let num1 = 0;
-let num2 = 0;
-let num3 = 0;
-const randomNum = function() {
-    num1 = Math.floor(Math.random() * 9);
-    num2 = Math.floor(Math.random() * 9);
-    num3 = Math.floor(Math.random() * 9);
-};
+let fruits = ["lemon", "apple", "pineapple", "watermelon", "pear", "mango", "dragonfruit","golden apple"];
+let num1;
+let num2;
+let num3;
 
 const numToFruit = num => {
     if (num > 7) {
@@ -16,3 +11,28 @@ const numToFruit = num => {
     return fruits[num];
     }
 };
+
+const randomNum = function() {
+    
+    num1 = Math.floor(Math.random() * 9);
+    num2 = Math.floor(Math.random() * 9);
+    num3 = Math.floor(Math.random() * 9);
+    
+};
+
+const fruitValues = (num1, num2, num3) => {
+    let fruit1 = numToFruit(num1);
+    let fruit2 = numToFruit(num2);
+    let fruit3 = numToFruit(num3);
+    return { fruit1, fruit2, fruit3 };
+};
+
+const run = function() {
+    randomNum()
+    let { fruit1, fruit2, fruit3 } = fruitValues(num1, num2, num3);
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+};
+
+run();
